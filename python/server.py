67f17a3bf3855ad7d4a8d516bfa8f03f8d0b7d3b
Andrew Lunde <andrew.lunde@sap.com>
2019-01-17 16:30:14 -0500

    Tidy up.

--- python/server.py
+++ python/server.py
@@ -9,15 +9,7 @@ from flask import Response
 from flask import send_from_directory
 #   
 import os
-#import pyhdb
-# Downloading pyhdb-0.3.3.tar.gz
 import json
-import datetime
-#import Crypto.PublicKey.RSA as RSA
-#import jws.utils
-#import python_jwt as jwt
-#https://help.sap.com/viewer/4505d0bdaf4948449b7f7379d24d0f0d/2.0.03/en-US/8732609bd5314b51a17d6a3cc09110c3.html#loio8732609bd5314b51a17d6a3cc09110c3__section_atx_2vt_vt
-from sap import xssec
 from cfenv import AppEnv
 #
 #from sap.cf_logging import flask_logging
@@ -38,26 +30,6 @@ env = AppEnv()
 port = int(os.getenv("PORT", 9099))
 hana = env.get_service(label='hana')
 
-def attach(port, host):
-    try:
-        import pydevd
-        pydevd.stoptrace() #I.e.: disconnect if already connected
-        # pydevd.DebugInfoHolder.DEBUG_RECORD_SOCKET_READS = True
-        # pydevd.DebugInfoHolder.DEBUG_TRACE_BREAKPOINTS = 3
-        # pydevd.DebugInfoHolder.DEBUG_TRACE_LEVEL = 3
-        pydevd.settrace(
-            port=port,
-            host=host,
-            stdoutToServer=True,
-            stderrToServer=True,
-            overwrite_prev_trace=True,
-            suspend=False,
-            trace_only_current_thread=False,
-            patch_multiprocessing=False,
-        )
-    except:
-        import traceback;traceback.print_exc() 
-        
         
 # This module's Flask webserver will respond to these three routes (URL paths)
 # If there is no path then just return Hello World and this module's instance number
@@ -78,13 +50,13 @@ def favicon():
 def python_links():
     output = '<strong>Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0)) + '</strong> Try these links.</br>\n'
     output += '<a href="/python/insert">/python/insert</a><br />\n'
+    output += '<a href="/python/retrieve">/python/retrieve</a><br />\n'
+    output += '<a href="/python/delete">/python/delete</a><br />\n'
     return output
 
 @app.route('/python/insert')
 def unauth_ss_insert():
     output = 'Python UnAuthorized SecureStore Insert. \n'
-    svcs_json = str(os.getenv("VCAP_SERVICES", 0))
-    svcs = json.loads(svcs_json)
 
     schema = hana.credentials['schema']
     host = hana.credentials['host']
@@ -131,27 +103,141 @@ def unauth_ss_insert():
     cursor = connection.cursor()
 
 #    # Form an SQL statement to retrieve some data
-    cursor.execute('SELECT "tempId", "tempVal", "ts", "created" FROM "data::sensors.temp"')
-    cursor.execute('SELECT "tempId", "tempVal", "ts", "created" FROM "data::sensors.temp"')
+    cursor.execute('call SYS.USER_SECURESTORE_INSERT (\'TestStoreName\', False, \'TestKey\', \'ab2467cdef\')')
 #call SYS.USER_SECURESTORE_INSERT ('TestStoreName', False, 'TestKey', 'ab2467cdef');
 #call SYS.USER_SECURESTORE_RETRIEVE ('TestStoreName', False, 'TestKey', ?);
 #call SYS.USER_SECURESTORE_DELETE ('TestStoreName', False, 'TestKey');
 
-#call SYS.USER_SECURESTORE_INSERT ('TestStoreName', False, 'TestKey2', TO_BINARY('Andrew!'));
-#call SYS.USER_SECURESTORE_RETRIEVE ('TestStoreName', False, 'TestKey2', BINTOSTR(?));
+#    # Close the DB connection
+    connection.close()
+#
+    # Return the results
+    return output
+
+@app.route('/python/retrieve')
+def unauth_ss_retrieve():
+    output = 'Python UnAuthorized SecureStore Retrieve. \n'
+
+    schema = hana.credentials['schema']
+    host = hana.credentials['host']
+    port = hana.credentials['port']
+    user = hana.credentials['user']
+    password = hana.credentials['password']
+
+    # The certificate will available for HANA service instances that require an encrypted connection
+    # Note: This was tested to work with python hdbcli-2.3.112 tar.gz package not hdbcli-2.3.14 provided in XS_PYTHON00_0-70003433.ZIP  
+    if 'certificate' in hana.credentials:
+        haascert = hana.credentials['certificate']
+    
+    output += 'schema: ' + schema + '\n'
+    output += 'host: ' + host + '\n'
+    output += 'port: ' + port + '\n'
+    output += 'user: ' + user + '\n'
+    output += 'pass: ' + password + '\n'
+
+#    # Connect to the python HANA DB driver using the connection info
+# User for HANA as a Service instances
+    if 'certificate' in hana.credentials:
+        connection = dbapi.connect(
+            address=host,
+            port=int(port),
+            user=user,
+            password=password,
+            currentSchema=schema,
+            encrypt="true",
+            sslValidateCertificate="true",
+            sslCryptoProvider="openssl",
+            sslTrustStore=haascert
+        )
+    else:
+        connection = dbapi.connect(
+            address=host,
+            port=int(port),
+            user=user,
+            password=password,
+            currentSchema=schema
+        )
+ 
+
+#    # Prep a cursor for SQL execution
+    cursor = connection.cursor()
+
+#    # Form an SQL statement to retrieve some data
+    cursor.execute('call SYS.USER_SECURESTORE_RETRIEVE (\'TestStoreName\', False, \'TestKey\', ?)')
+#call SYS.USER_SECURESTORE_INSERT ('TestStoreName', False, 'TestKey', 'ab2467cdef');
+#call SYS.USER_SECURESTORE_RETRIEVE ('TestStoreName', False, 'TestKey', ?);
+#call SYS.USER_SECURESTORE_DELETE ('TestStoreName', False, 'TestKey');
 
 #    # Execute the SQL and capture the result set
     results = cursor.fetchall()
 #
 #    # Loop through the result set and output
-    for sensor_val in sensor_vals:
-        output += 'sensor_val: ' + str(sensor_val[1]) + ' at: ' + str(sensor_val[2]) + '\n'
+    for result in results:
+        output += 'result: ' + str(result[1]) + '\n'
 #
 #    # Close the DB connection
     connection.close()
 #
     # Return the results
     return output
+
+@app.route('/python/delete')
+def unauth_ss_delete():
+    output = 'Python UnAuthorized SecureStore Delete. \n'
+
+    schema = hana.credentials['schema']
+    host = hana.credentials['host']
+    port = hana.credentials['port']
+    user = hana.credentials['user']
+    password = hana.credentials['password']
+
+    # The certificate will available for HANA service instances that require an encrypted connection
+    # Note: This was tested to work with python hdbcli-2.3.112 tar.gz package not hdbcli-2.3.14 provided in XS_PYTHON00_0-70003433.ZIP  
+    if 'certificate' in hana.credentials:
+        haascert = hana.credentials['certificate']
+    
+    output += 'schema: ' + schema + '\n'
+    output += 'host: ' + host + '\n'
+    output += 'port: ' + port + '\n'
+    output += 'user: ' + user + '\n'
+    output += 'pass: ' + password + '\n'
+
+#    # Connect to the python HANA DB driver using the connection info
+# User for HANA as a Service instances
+    if 'certificate' in hana.credentials:
+        connection = dbapi.connect(
+            address=host,
+            port=int(port),
+            user=user,
+            password=password,
+            currentSchema=schema,
+            encrypt="true",
+            sslValidateCertificate="true",
+            sslCryptoProvider="openssl",
+            sslTrustStore=haascert
+        )
+    else:
+        connection = dbapi.connect(
+            address=host,
+            port=int(port),
+            user=user,
+            password=password,
+            currentSchema=schema
+        )
+ 
+
+#    # Prep a cursor for SQL execution
+    cursor = connection.cursor()
+
+#    # Form an SQL statement
+    cursor.execute('call SYS.USER_SECURESTORE_DELETE (\'TestStoreName\', False, \'TestKey\')')
+
+#    # Close the DB connection
+    connection.close()
+#
+    # Return the results
+    return output
+
 
 if __name__ == '__main__':
     # Run the app, listening on all IPs with our chosen port number
