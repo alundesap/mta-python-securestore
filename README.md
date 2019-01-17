0e36341d2e164a418e1790a3d0c898b356ff0f99
Andrew Lunde <andrew.lunde@sap.com>
2019-01-17 21:10:34 +0000

    changs

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mta-python-securestore
 Multi-Target Application Python Example of interfacing with the Secure Store API
+
+wget -c http://thedrop.sap-a-team.com/files/hana_ml-1.0.3.tar.gz ; wget -c http://thedrop.sap-a-team.com/files/XS_PYTHON00_1-70003433.ZIP ; mkdir -p sap_dependencies ; unzip XS_PYTHON00_1-70003433.ZIP -d sap_dependencies
+
+cd python ; mkdir -p vendor ; pip download -d vendor -r requirements.txt --find-links ../sap_dependencies --find-links ../hana_ml-1.0.3.tar.gz ; cd ..
+
--- python/server.py
+++ python/server.py
@@ -65,10 +65,7 @@ def attach(port, host):
 @app.route('/')
 def hello_world():
     output = '<strong>Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0)) + '</strong> Try these links.</br>\n'
-    output += '<a href="/env">/env</a><br />\n'
-    output += '<a href="/python/test">/python/test</a><br />\n'
-    output += '<a href="/python/db_only">/python/db_only</a><br />\n'
-    output += '<a href="/auth_python/db_valid">/auth_python/db_valid</a><br />\n'
+    output += '<a href="/python/links">/python/links</a><br />\n'
     return output
     
 # Satisfy browser requests for favicon.ico so that don't return 404
@@ -76,94 +73,16 @@ def hello_world():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/env')
-def dump_env():
-    output = '\n Key Environment variables... \n'
-    output += 'PYTHONHOME: ' + str(os.getenv("PYTHONHOME", 0)) + '\n'
-    output += 'PYTHONPATH: ' + str(os.getenv("PYTHONPATH", 0)) + '\n'
-    output += 'VCAP_SERVICES: ' + str(os.getenv("VCAP_SERVICES", 0)) + '\n'
-    output += 'host: ' + hana.credentials['host'] + '\n'
-    output += 'port: ' + hana.credentials['port'] + '\n'
-    output += 'user: ' + hana.credentials['user'] + '\n'
-    output += 'pass: ' + hana.credentials['password'] + '\n'
-    output += '\n'
-    return output
-
 # Coming through the app-router
 @app.route('/python/links')
 def python_links():
     output = '<strong>Hello World! I am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0)) + '</strong> Try these links.</br>\n'
-    output += '<a href="/python/test">/python/test</a><br />\n'
-    output += '<a href="/python/db_only">/python/db_only</a><br />\n'
-    output += '<a href="/auth_python/db_valid">/auth_python/db_valid</a><br />\n'
+    output += '<a href="/python/insert">/python/insert</a><br />\n'
     return output
 
-# If there is a request for a python/test, return Testing message and module's instance number
-@app.route('/python/test')
-def unauth_test():
-    return 'Python UnAuthorized Test, Yo! <br />\nI am instance ' + str(os.getenv("CF_INSTANCE_INDEX", 0))
-
-@app.route('/python/post', methods=['POST'])
-def unauth_post():
-    output = 'Python Post to DB (Dangerous!). \n'
-    output += '\n'
-    output += 'Receiving module should check that it came from our approuter and verify or abort if otherwise.\n'
-    output += '\n'
-
-    content = request.json
-
-    output += content
-
-    return Response(output, mimetype='application/json' , status=201,)
-
-@app.route('/python/set_env')
-def set_pyenv():
-    output = '\n Set Environment variable... \n'
-    if request.args.get('PATHS_FROM_ECLIPSE_TO_PYTHON'):
-        output += request.args.get('PATHS_FROM_ECLIPSE_TO_PYTHON')
-        os.environ["PATHS_FROM_ECLIPSE_TO_PYTHON"] = request.args.get('PATHS_FROM_ECLIPSE_TO_PYTHON')
-        output += '\n'
-        output += 'Eclipse paths set for debugging.\n'
-        output += '\n'
-    output += '\n'
-    return Response(output, mimetype='text/plain' , status=200,)
-
-@app.route('/python/env')
-def dump_pyenv():
-    output = '\n Key Environment variables... \n'
-    output += 'PYTHONHOME: ' + str(os.getenv("PYTHONHOME", 0)) + '\n'
-    output += 'PYTHONPATH: ' + str(os.getenv("PYTHONPATH", 0)) + '\n'
-    output += 'PATHS_FROM_ECLIPSE_TO_PYTHON: ' + str(os.getenv("PATHS_FROM_ECLIPSE_TO_PYTHON", 0)) + '\n'
-    jsonok = json.loads(os.environ.get('PATHS_FROM_ECLIPSE_TO_PYTHON', '[]'))
-    if jsonok:
-        output += "JSON is OK" + '\n'
-        tuples = [tuple(x) for x in jsonok]
-    else:
-        output += "JSON is NOT OK" + '\n'
-    output += 'VCAP_SERVICES: ' + str(os.getenv("VCAP_SERVICES", 0)) + '\n'
-    output += 'host: ' + hana.credentials['host'] + '\n'
-    output += 'port: ' + hana.credentials['port'] + '\n'
-    output += 'user: ' + hana.credentials['user'] + '\n'
-    output += 'pass: ' + hana.credentials['password'] + '\n'
-    output += '\n'
-    return output
-
-@app.route('/python/attach')
-def do_attach():
-    output = '\n Attaching to debugger... \n'
-    attach(5678,"localhost")
-    output += '\n Set some breakpoints...\n'
-    return output
-
-# If there is a request for a python/test2, return Testing message and then check JWT and connect to the data service and retrieve some data
-@app.route('/python/db_only')
-def unauth_db_only():
-    output = 'Python UnAuthorized DB Only. \n'
-    #Enable to trigger debugging
-    os.environ["PATHS_FROM_ECLIPSE_TO_PYTHON"] = "[['/Users/i830671/git/mta_python_dev_env/python','/home/vcap/app']]"
-    output += '\n'
-    output += 'Receiving module should check that it came from our approuter and verify or abort if otherwise.\n'
-    output += '\n'
+@app.route('/python/insert')
+def unauth_ss_insert():
+    output = 'Python UnAuthorized SecureStore Insert. \n'
     svcs_json = str(os.getenv("VCAP_SERVICES", 0))
     svcs = json.loads(svcs_json)
 
@@ -213,149 +132,16 @@ def unauth_db_only():
 
 #    # Form an SQL statement to retrieve some data
     cursor.execute('SELECT "tempId", "tempVal", "ts", "created" FROM "data::sensors.temp"')
+    cursor.execute('SELECT "tempId", "tempVal", "ts", "created" FROM "data::sensors.temp"')
+#call SYS.USER_SECURESTORE_INSERT ('TestStoreName', False, 'TestKey', 'ab2467cdef');
+#call SYS.USER_SECURESTORE_RETRIEVE ('TestStoreName', False, 'TestKey', ?);
+#call SYS.USER_SECURESTORE_DELETE ('TestStoreName', False, 'TestKey');
+
+#call SYS.USER_SECURESTORE_INSERT ('TestStoreName', False, 'TestKey2', TO_BINARY('Andrew!'));
+#call SYS.USER_SECURESTORE_RETRIEVE ('TestStoreName', False, 'TestKey2', BINTOSTR(?));
 
 #    # Execute the SQL and capture the result set
-    sensor_vals = cursor.fetchall()
-#
-#    # Loop through the result set and output
-    for sensor_val in sensor_vals:
-        output += 'sensor_val: ' + str(sensor_val[1]) + ' at: ' + str(sensor_val[2]) + '\n'
-#
-#    # Close the DB connection
-    connection.close()
-#
-    # Return the results
-    return output
-
-# If there is a request for a python/test2, return Testing message and then check JWT and connect to the data service and retrieve some data
-@app.route('/auth_python/db_valid')
-def auth_db_valid():
-    output = 'Python Authorized DB Validated Request. \n'
-    output += '\n'
-    output += 'Receiving module should check that it came from our approuter and verify or abort if otherwise.\n'
-    output += '\n'
-    svcs_json = str(os.getenv("VCAP_SERVICES", 0))
-    svcs = json.loads(svcs_json)
-
-    # Verify the JWT before proceeding. or refuse to process the request.
-    # https://jwt.io/ JWT Debugger Tool and libs for all languages
-    # https://github.com/jpadilla/pyjwt/
-    # https://github.com/davedoesdev/python-jwt
-
-    # From the vcap_services environment variable pull out these things for later.
-#    vkey = svcs["xsuaa"][0]["credentials"]["verificationkey"]
-#    secret = svcs["xsuaa"][0]["credentials"]["clientsecret"]
-#
-#    #output += 'vkey: ' + vkey + '\n'
-#    #output += 'secret: ' + secret + '\n'
-#
-#    #jwt.decode(encoded, verify=False)
-#    req_host = request.headers.get('Host')
-#    req_auth = request.headers.get('Authorization')
-#
-#    #output += 'req_host: ' + req_host + '\n'
-#    #output += 'req_auth: ' + req_auth + '\n'
-#
-#    #import jwt
-#    #output += 'req_auth = ' + req_auth + '\n'
-#
-#    #Check to see if the request has an authorization header and if it starts with "Bearer "
-#    if req_auth:
-#        if req_auth.startswith("Bearer "):
-#            output += 'JWT Authorization is of type Bearer! \n'
-#        else:
-#            output += 'JWT Authorization is not of type Bearer! \n'
-#    else:
-#        output += 'Authorization header is missing! \n'
-#
-#    output += '\n'
-#
-#    #If it looks like the right type of authoriztion header, grab it's contents.
-#    if req_auth:
-#        jwtoken = req_auth[7:]
-#
-#        # The PKEY in the env has the \n stripped out and the importKey expects them!
-#        pub_pem = "-----BEGIN PUBLIC KEY-----\n" + vkey[26:-24] + "\n-----END PUBLIC KEY-----\n"
-#        #output += 'pub_pem = ' + pub_pem + '\n'
-#
-#	# Manipulate the pem key so that we can verify it.
-#        pub_key = RSA.importKey(pub_pem)
-#        (header, claim, sig) = jwtoken.split('.')
-#        header = jws.utils.from_base64(header)
-#        claim = jws.utils.from_base64(claim)
-#        if jws.verify(header, claim, sig, pub_key, is_json=True):
-#            output += 'JWT is Verified! \n'
-#        else:
-#            output += 'JWT FAILED Verification! \n'
-#
-#    else:
-#    else:
-#        output += 'Normally we would only do work if JWT is verified.\n'
-#
-#    output += '\n'
-
-    uaa_service = env.get_service(label='xsuaa').credentials
-    access_token = request.headers.get('authorization')[7:]
-
-    security_context = xssec.create_security_context(access_token, uaa_service)
-    isAuthorized = security_context.check_scope('openid')
-    if not isAuthorized:
-        abort(403)
-
-    output += 'get_logon_name: ' + security_context.get_logon_name() + '\n'
-#    output += 'get_given_name: ' + security_context.get_given_name() + '\n'
-#    output += 'get_family_name: ' + security_context.get_family_name() + '\n'
-    output += 'get_email: ' + security_context.get_email() + '\n'
-#    output += 'get_subdomain: ' + security_context.get_subdomain() + '\n'
-#    output += 'get_clientid: ' + security_context.get_clientid() + '\n'
-    output += 'get_identity_zone: ' + security_context.get_identity_zone() + '\n'
-#    output += 'get_grant_type: ' + security_context.get_grant_type() + '\n'
-    
-#
-#    # This module should only proced with any further execution if the JWT has been verified.
-#    # In this example we blindly continue, but this is not the best practice.
-#
-#    # Grab information from the vcap_services about the database connection
-#    schema = svcs["hana"][0]["credentials"]["schema"]
-#    user = svcs["hana"][0]["credentials"]["user"]
-#    password = svcs["hana"][0]["credentials"]["password"]
-#    conn_str = svcs["hana"][0]["credentials"]["url"]
-#    host = svcs["hana"][0]["credentials"]["host"]
-#    port = svcs["hana"][0]["credentials"]["port"]
-#    driver = svcs["hana"][0]["credentials"]["driver"]
-#
-    schema = hana.credentials['schema']
-    host = hana.credentials['host']
-    port = hana.credentials['port']
-    user = hana.credentials['user']
-    password = hana.credentials['password']
-    
-
-    output += 'schema: ' + schema + '\n'
-    output += 'host: ' + host + '\n'
-    output += 'port: ' + port + '\n'
-    output += 'user: ' + user + '\n'
-    output += 'pass: ' + password + '\n'
-
-#    output += 'schema: ' + schema + '\n'
-#    output += 'user: ' + user + '\n'
-#    output += 'password: ' + password + '\n'
-#    output += 'conn_str: ' + conn_str + '\n'
-#    output += 'host: ' + host + '\n'
-#    output += 'port: ' + port + '\n'
-#    output += 'driver: ' + driver + '\n'
-#
-#    output += '\n'
-#    # Connect to the python HANA DB driver using the connection info
-#    connection = pyhdb.connect(host,int(port),user,password)
-    connection = dbapi.connect(host,int(port),user,password)
-#    connection = dbapi.connect(addresst=host,port=int(port),user=user,password=password)
-#    # Prep a cursor for SQL execution
-    cursor = connection.cursor()
-#    # Form an SQL statement to retrieve some data
-    cursor.execute('SELECT "tempId", "tempVal", "ts", "created" FROM "' + schema + '"."DAT368.db.data::sensors.temp"')
-#    # Execute the SQL and capture the result set
-    sensor_vals = cursor.fetchall()
+    results = cursor.fetchall()
 #
 #    # Loop through the result set and output
     for sensor_val in sensor_vals:
@@ -373,4 +159,5 @@ if __name__ == '__main__':
     #app.run(host='0.0.0.0', port=port)
     # Use this for debugging 
     app.run(debug=True, host='0.0.0.0', port=port)
+
 
